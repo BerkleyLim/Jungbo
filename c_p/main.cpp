637cb91ba1d0c22107adb53f1226aafacfb28884
BerkleyLim <berkleylim16@gmail.com>
2024-03-30 23:46:25 +0900

    feat: upload c project - 2023년 4월 기술문제 업로드

--- c_p/main.cpp
+++ c_p/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+/**
+ * question1
+ * 2023년 4월 기출문제
+ */
+#include<stdio.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char a[] = "Art";   // a = Art
+    char* p = NULL;     // p = Null
+    p = a;              // p = Art
+    printf("%s\n",a);   // Art
+    printf("%c\n", *p); // A
+    printf("%c\n", *a); // A
+    printf("%s\n", p);  // Art
+    for (int i = 0; a[i] != '\0'; i++) { // \0 -> null
+        printf("%c", a[i]);
+    } // Art
 }
+
+//#include <iostream>
+
+//int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+//}
---
+++ c_p/question1/README.md
@@ -0,0 +1,10 @@
+# Question1
+
+```c++
+int main() {
+    char a[] = "Art";
+    char* p = NULL;
+    p = a;
+    printf("%s\n",a);
+}
+```
