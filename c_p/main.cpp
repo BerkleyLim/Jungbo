f71804a2ef7aa086405eefaed4c938be841c6d1b
BerkleyLim <berkleylim16@gmail.com>
2024-03-31 17:16:32 +0900

    feat: upload c project - 2023년 4월 기술문제 업로드

--- c_p/main.cpp
+++ c_p/main.cpp
@@ -1,18 +1,47 @@
 /**
+ * question3
+ * 2023년 4월 기출 문제
+ */
+#include<stdio.h>
+void swap(int *a, int idx1, int idx2) {
+    int t = a[idx1];
+    a[idx1] = a[idx2];
+    a[idx2] = t;
+}
+
+void Usort(int *a, int len) {
+    for (int i = 0; i < len-1; i++)
+        for (int j = 0; j <len - 1 -i; j++)
+            if(a[j] > a[j+1])
+                swap(a, j ,j+1);
+}
+
+int main() {
+    int a[] = {85, 75, 50, 100, 95};
+    int nx = 5;
+    Usort(a, nx);
+
+    for (int i = 0; i < nx; i++) {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+
+/**
  * question2
  * 2023년 4월 기출문제
  */
-#include<stdio.h>
-
-int main() {
-    char* a = "qwer";
-    char* b = "qwtety";
-
-    for (int i = 0; a[i] != '\0'; i++)
-        for (int j = 0; b[j] != '\0'; j++)
-            if (a[i] == b[j])
-                printf("%c", a[i]);
-}
+//#include<stdio.h>
+//
+//int main() {
+//    char* a = "qwer";
+//    char* b = "qwtety";
+//
+//    for (int i = 0; a[i] != '\0'; i++)
+//        for (int j = 0; b[j] != '\0'; j++)
+//            if (a[i] == b[j])
+//                printf("%c", a[i]);
+//}
 
 
 /**
